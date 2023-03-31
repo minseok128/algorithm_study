2309f19608ab672e3ce790bcb47ed344067b0dc9
minseok128 <minseok128128@gmail.com>
2023-03-31 12:40:36 +0900

    Edit 1074

--- cpp/20230330/_1074.cpp
+++ cpp/20230330/_1074.cpp
@@ -1,12 +1,7 @@
 #include <iostream>
 using namespace std;
-
 int main()
 {
-	cin.tie(0);
-	cout.tie(0);
-	ios::sync_with_stdio(0);
-
 	int n, r, c, result = 0;
 	cin >> n >> c >> r;
 	for (int i = n; i > 0; i--)
