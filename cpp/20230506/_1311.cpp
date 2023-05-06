5057adafd8a5f83e3cf7477a3936052ffeb6762e
minseok128 <minseok128128@gmail.com>
2023-05-06 23:03:16 +0900

    Edit 1311

--- cpp/20230506/_1311.cpp
+++ cpp/20230506/_1311.cpp
@@ -20,11 +20,8 @@ int main()
 		dp[0][1 << i] = cost[0][i];
 
 	for (int i = 1; i < n; i++)
-	{
 		for (int j = 0; j < n; j++)
-		{
 			for (int k = 0; k < 1 << n; k++)
-			{
 				if (dp[i - 1][k] && !(k & (1 << j)))
 				{
 					if (dp[i][k | (1 << j)])
@@ -32,8 +29,5 @@ int main()
 					else
 						dp[i][k | (1 << j)] = dp[i - 1][k] + cost[i][j];
 				}
-			}
-		}
-	}
 	cout << dp[n - 1][(1 << n) - 1] << '\n';
 }
