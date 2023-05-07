95899ccd78845d908600b2aa4774587a72ea785f
minseok128 <minseok128128@gmail.com>
2023-05-08 00:16:25 +0900

    Edit 1311

--- cpp/20230506/_1311.cpp
+++ cpp/20230506/_1311.cpp
@@ -12,23 +12,22 @@ int main()
 	int n;
 	cin >> n;
 	vector<vector<int>> dp(n, vector<int>(1 << 20));
-	vector<vector<int>> cost(n, vector<int>(n));
+	vector<vector<short>> cost(n, vector<short>(n));
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < n; j++)
-			cin >> cost[j][i];
+			cin >> cost[i][j];
 	for (int i = 0; i < n; i++)
 		dp[0][1 << i] = cost[0][i];
 
 	for (int i = 1; i < n; i++)
 		for (int j = 0; j < n; j++)
-			if (cost[i][j])
-				for (int k = 0; k < 1 << n; k++)
-					if (dp[i - 1][k] && !(k & (1 << j)))
-					{
-						if (dp[i][k | (1 << j)])
-							dp[i][k | (1 << j)] = min(dp[i][k | (1 << j)], dp[i - 1][k] + cost[i][j]);
-						else
-							dp[i][k | (1 << j)] = dp[i - 1][k] + cost[i][j];
-					}
+			for (int k = 0; k < 1 << n; k++)
+				if (dp[i - 1][k] && !(k & (1 << j)))
+				{
+					if (dp[i][k | (1 << j)])
+						dp[i][k | (1 << j)] = min(dp[i][k | (1 << j)], dp[i - 1][k] + cost[i][j]);
+					else
+						dp[i][k | (1 << j)] = dp[i - 1][k] + cost[i][j];
+				}
 	cout << dp[n - 1][(1 << n) - 1] << '\n';
 }
