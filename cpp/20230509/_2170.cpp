66eb25c06536a7e20bc2f12e3667dae8ff953692
michang <minseok128128@gmail.com>
2023-05-09 17:20:45 +0900

    Solve 2170

---
+++ cpp/20230509/_2170.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	cin.tie(0);
+	cout.tie(0);
+	ios::sync_with_stdio(0);
+
+	int N;
+	cin >> N;
+	vector<pair<int, int>> arr(N);
+
+	for (int i = 0; i < N; i++)
+		cin >> arr[i].first >> arr[i].second;
+	sort(arr.begin(), arr.end());
+	cout << "\n\n";
+	
+	vector<pair<int, int>> sarr;
+	sarr.emplace_back(arr[0]);
+	for (int i = 1, j = 1; i < N; i++)
+	{
+		if (sarr[j - 1].second >= arr[i].first)
+		{
+			if (sarr[j - 1].second < arr[i].second)
+				sarr[j - 1].second = arr[i].second;
+		}
+		else
+		{
+			sarr.emplace_back(arr[i]);
+			j++;
+		}
+
+	}
+
+	long long res = 0;
+
+	for (int i = 0; i < sarr.size(); i++)
+		cout << sarr[i].first << ',' << sarr[i].second << '\n';
+	for (int i = 0; i < sarr.size(); i++)
+		res += sarr[i].second - sarr[i].first;
+	cout << res << '\n';
+}
