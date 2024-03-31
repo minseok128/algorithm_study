b4dac4969a4e23d4fef0d72a918d42942b65175d
michang <minseok128128@gmail.com>
2024-03-31 18:05:37 +0900

    Added assignment-1 : insertion sort 구현

---
+++ assignments/assignment-1/insertion.cpp
@@ -0,0 +1,22 @@
+#include "sorting.hpp"
+
+void	insertion_sort(vector<int> arr, ofstream &ofile)
+{
+	auto	start = high_resolution_clock::now();
+	int		size = arr.size();
+
+	for (int i = 1; i < size; ++i)
+	{
+		int	key = arr[i];
+		int	j = i - 1;
+
+		while (j >= 0 && arr[j] > key)
+		{
+			arr[j + 1] = arr[j];
+			--j;
+		}
+		arr[j + 1] = key;
+	}
+
+	print_array(arr, ofile, start);
+}
