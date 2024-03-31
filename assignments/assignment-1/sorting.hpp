79cfece176a01d580b2ba017245d2e82acebe73e
michang <minseok128128@gmail.com>
2024-03-31 17:22:07 +0900

    Added assignment-1 : initial header file

---
+++ assignments/assignment-1/sorting.hpp
@@ -0,0 +1,13 @@
+#ifndef SORTING_HPP
+# define SORTING_HPP
+# include <iostream>
+# include <vector>
+
+using namespace std;
+
+void	insertion_sort(vector<int> arr);
+void	merge_sort(vector<int> arr);
+void	merge_insertion_sort(vector<int> arr);
+void	print_array(vector<int> &arr);
+
+#endif
