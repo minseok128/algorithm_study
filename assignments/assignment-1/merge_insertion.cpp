5acddefad9e6e085bd2f118bdb213a0ed87306b2
michang <minseok128128@gmail.com>
2024-04-01 20:21:41 +0900

    Added merge_insertion sort

---
+++ assignments/assignment-1/merge_insertion.cpp
@@ -0,0 +1,72 @@
+#include "sorting.hpp"
+
+static void	mi_insertion(vector<int> &base_arr, ofstream &ofile)
+{
+	auto		start = high_resolution_clock::now();
+	vector<int>	arr(base_arr);
+	int			size = arr.size();
+	int			key, i, j;
+
+	for (i = 1; i < size; ++i)
+	{
+		key = arr[i];
+		j = i - 1;
+		while (j >= 0 && key < arr[j])
+		{
+			arr[j + 1] = arr[j];
+			--j;
+		}
+		arr[j + 1] = key;
+	}
+
+	print_array("insertion", arr, ofile, start);
+	ofile << '\n';
+}
+
+static void	mi_conquer(vector<int> &arr, vector<int> &tmp, int left, int right, int mid)
+{
+	int			i = 0, l = left, r = mid + 1;
+
+	while (l <= mid && r <= right)
+	{
+		if (arr[l] > arr[r])
+			tmp[i++] = arr[r++];
+		else
+			tmp[i++] = arr[l++];
+	}
+
+	if (l > mid)
+		while (r <= right)
+			tmp[i++] = arr[r++];
+
+	if (r > right)
+		while (l <= mid)
+			tmp[i++] = arr[l++];
+
+	for (i = left; i <= right; i++)
+		arr[i] = tmp[i - left];
+}
+
+static void	mi_devide(vector<int> &arr, vector<int> &tmp, int l, int r)
+{
+	int	m;
+
+	if (l >= r)
+		return ;
+	m = (l + r) / 2;
+	mi_devide(arr, tmp, l, m);
+	mi_devide(arr, tmp, m + 1, r);
+	mi_conquer(arr, tmp, l, r, m);
+}
+
+void	merge_insertion_sort(vector<int> &base_arr, ofstream &ofile)
+{
+	auto		start = high_resolution_clock::now();
+	int			size = base_arr.size();
+	vector<int>	arr(base_arr), tmp(size);
+
+	mi_devide(arr, tmp, 0, size - 1);
+
+	print_array("merge_insertion", arr, ofile, start);
+	ofile << '\n';
+}
