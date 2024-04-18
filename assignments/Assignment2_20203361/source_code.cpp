785d225f1969e2f442b517bac34031bb533a3b20
minseok128 <minseok128128@gmail.com>
2024-04-18 11:22:47 +0900

    Update assignment2 : comments

--- assignments/Assignment2_20203361/source_code.cpp
+++ assignments/Assignment2_20203361/source_code.cpp
@@ -14,14 +14,14 @@ static void	print_array(string type, vector<int> &arr,
 	ofstream &ofile, high_resolution_clock::time_point &start);
 
 // --------normal quick sort & randomized quick sort--------
-random_device rd;
-mt19937 gen(rd());
-uniform_int_distribution<> dis;
+random_device				rd;  // 난수 생성 장치 초기화
+mt19937						gen(rd()); // 난수 생성기
+uniform_int_distribution<>	dis; // 균일 분포로 난수 생성
 
 // --------randomized pivot--------
 int	q2_partition(vector<int> &arr, int l, int r)
 {
-	swap(arr[r], arr[(unsigned int)dis(gen) % (r - l) + l]);
+	swap(arr[r], arr[(unsigned int)dis(gen) % (r - l) + l]); // 무작위 피벗 선택 및 스왑
 
 	int	pivot = arr[r];
 	int	i = l - 1;
@@ -39,7 +39,7 @@ int	q2_partition(vector<int> &arr, int l, int r)
 // --------right most pivot--------
 int	q1_partition(vector<int> &arr, int l, int r)
 {
-	int	pivot = arr[r];
+	int	pivot = arr[r]; // 오른쪽 피벗 사용
 	int	i = l - 1;
 
 	for (int j = l; j < r; ++j)
@@ -60,8 +60,8 @@ void	quick_divied(vector<int> &arr, int l, int r,
 	if (l >= r)
 		return ;
 	p = f(arr, l, r);
-	quick_divied(arr, l, p - 1, f);
-	quick_divied(arr, p + 1, r, f);
+	quick_divied(arr, l, p - 1, f); // 왼쪽 부분 배열 정렬
+	quick_divied(arr, p + 1, r, f); // 오른쪽 부분 배열 정렬
 }
 
 void	quick_sort(vector<int> &base_arr, ofstream &ofile,
@@ -71,7 +71,7 @@ void	quick_sort(vector<int> &base_arr, ofstream &ofile,
 	int			size = base_arr.size();
 	vector<int>	arr(base_arr);
 
-	quick_divied(arr, 0, size - 1, f);
+	quick_divied(arr, 0, size - 1, f); 
 
 	print_array("time", arr, ofile, start);
 	ofile << '\n';
@@ -104,7 +104,7 @@ void	tr_quick_sort(vector<int> &base_arr, ofstream &ofile)
 	int			size = base_arr.size();
 	vector<int>	arr(base_arr);
 
-	tr_quick_divied(arr, 0, size - 1);
+	tr_quick_divied(arr, 0, size - 1); // 꼬리 재귀 최적화 퀵정렬 실행
 
 	print_array("time", arr, ofile, start);
 }
@@ -148,9 +148,9 @@ int main(int ac, char **av)
 		ifile >> base_arr[i];
 	ifile.close();
 
-	quick_sort(base_arr, ofile, q1_partition);
-	quick_sort(base_arr, ofile, q2_partition);
-	tr_quick_sort(base_arr, ofile);
+	quick_sort(base_arr, ofile, q1_partition); // 일반 퀵정렬 실행
+	quick_sort(base_arr, ofile, q2_partition); // 무작위 피벗 퀵정렬 실행
+	tr_quick_sort(base_arr, ofile); // 꼬리 재귀 최적화 퀵정렬 실행
 
 	ofile.close();
 	return (0);
